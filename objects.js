56d11dae992ebb89941ce4e4ae4f78df6c24176f
Berry  Semexan <bsemexan@gmail.com>
2018-04-20 19:46:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {akon:"selfish"};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = [songTitle];
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
